999358dba6d3ff1a55995da9e6a1118e549ad3e3
NStephenson <nickstephenson90@gmail.com>
2016-04-25 14:11:26 -0400

    wrote a test, made it pass

--- tests/MainController.spec.js
+++ tests/MainController.spec.js
@@ -1,5 +1,16 @@
-describe('example test', function() {
-    it('should be true', function() {
-        expect('foo').toBe('foo');
-    });
+describe('MainController', function() {
+  beforeEach(module('app'));
+
+  var controller;
+  beforeEach(inject(function(_$controller_){
+    $controller = _$controller_;
+  }));
+
+  it('should have Steve Job\'s name', function() {
+    var $scope;
+    var controller = $controller('MainController', {scope: $scope});
+    expect(controller.name).toBe('Steve Jobs');
+  });
+  
 });
+
